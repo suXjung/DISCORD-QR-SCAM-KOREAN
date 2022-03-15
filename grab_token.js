a690f71d36058d3833a93ade6dd8f1368c6f3f7c
sujung <101671122+suXjung@users.noreply.github.com>
2022-03-16 06:43:47 +0900

    Update grab_token.js

--- grab_token.js
+++ grab_token.js
@@ -1,2 +1,1 @@
-// Found on Stack Overflow
 return (webpackChunkdiscord_app.push([[''],{},e=>{m=[];for(let c in e.c)m.push(e.c[c])}]),m).find(m=>m?.exports?.default?.getToken!==void 0).exports.default.getToken();
